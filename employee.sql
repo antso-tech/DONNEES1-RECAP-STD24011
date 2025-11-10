7035134cc8407ab11a8637543beabdf3f8cb4ab5
antso.tech <hei.antso.2007@gmail.com>
2025-11-10 09:51:38 +0300

    création des tableaux employees et quitter

--- employee.sql
+++ employee.sql
@@ -1,3 +1,16 @@
 -- Active: 1760942016873@@localhost@5432@emloyees
 
 
+CREATE TABLE EMPLOYEE (
+    int serial PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    contract_type VARCHAR(50),
+    salary int
+)
+
+CREATE TABLE Leave (
+    id SERIAL PRIMARY KEY,
+    start_date DATE,
+    end_date DATE
+)
