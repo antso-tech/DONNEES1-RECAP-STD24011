4c1c8206593008134767fbe0aaab28fee860b168
antso.tech <hei.antso.2007@gmail.com>
2025-11-10 15:35:30 +0300

    ajout d'un nouveau tableau et des foreign key

--- employee.sql
+++ employee.sql
@@ -2,15 +2,26 @@
 
 
 CREATE TABLE EMPLOYEE (
-    int serial PRIMARY KEY,
+    id_employee serial PRIMARY KEY,
     first_name VARCHAR(100),
     last_name VARCHAR(100),
     contract_type VARCHAR(50),
-    salary int
+    salary int,
+    id_team int,
+    CONSTRAINT fk_team FOREIGN KEY (id_team) REFERENCES TEAM(id_team)
 )
 
 CREATE TABLE Leave (
-    id SERIAL PRIMARY KEY,
+    id_leave SERIAL PRIMARY KEY,
     start_date DATE,
-    end_date DATE
+    end_date DATE,
+    id_employee int,
+    CONSTRAINT fk_employee FOREIGN KEY (id_employee) REFERENCES EMPLOYEE(id_employee)
 )
+
+CREATE TABLE TEAM (
+    id_team SERIAL PRIMARY KEY,
+    name VARCHAR(50)
+)
+
+
