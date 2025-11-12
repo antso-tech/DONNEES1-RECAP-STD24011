5e49c07abb7370afb4a92cccb86cbdade0e7c175
antso.tech <hei.antso.2007@gmail.com>
2025-11-12 17:42:45 +0300

    ajout de nouvelles requetes

--- employee.sql
+++ employee.sql
@@ -43,3 +43,17 @@ INNER JOIN TEAM t
 on e.id_team = t.id_team 
 INNER JOIN LEAVE l 
 on e.id_employee = l.id_employee 
+
+SELECT  e.contract_type, count(*) as number_employee FROM EMPLOYEE e GROUP BY e.contract_type 
+
+SELECT start_date, end_date 
+FROM EMPLOYEE e inner 
+join LEAVE l 
+on e.id_employee = l.id_employee 
+WHERE start_date <= CURRENT_DATE 
+AND end_date >= CURRENT_DATE
+
+SELECT e.id_employee, e.first_name, e.last_name, t.name 
+FROM EMPLOYEE e inner 
+JOIN TEAM t ON e.id_team = t.id_team inner join LEAVE l on e.id_employee = l.id_employee WHERE start_date <= CURRENT_DATE 
+AND end_date >= CURRENT_DATE
